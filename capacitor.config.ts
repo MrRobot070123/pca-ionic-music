77d7276348e4e6dee7ae2466786c9400ed68a5b0
MrRobot070123 <dapacheco33@hotmail.com>
2025-07-09 13:15:50 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pca-ionic-music',
+  webDir: 'www'
+};
+
+export default config;
